e2cfa0e08438ccd9732dcc6c85dd6108ebb9f87b
takusan23 <takumu1200@gmail.com>
2023-01-28 18:24:18 +0900

    typescript

--- App.tsx
+++ App.tsx
@@ -2,10 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>ReactNative Expo 入門</Text>
+      <StatusBar hidden />
     </View>
   );
 }
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
